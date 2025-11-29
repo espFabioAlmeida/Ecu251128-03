70750dd015fd571d87084a68bfaf72194f0afc7b
Fábio Almeida <teg.fabio.almeida@gamil.com>
2025-11-28 21:03:13 -0300

    V1: Entradas Digitais

--- SubrotinasEntradasDigitais.c
+++ SubrotinasEntradasDigitais.c
@@ -8,7 +8,51 @@
 LEITURA ENTRADAS DIGITAIS
 ==============================================================================*/
 void leituraEntradasDigitais() {
+    if(flagSeguranca) {
+        if(debounceInverso(IN1)) {
+            flagSeguranca = false;
+        }
+    }
+    else {
+        if(debounce(IN1)) {
+            flagSeguranca = true;
+        }
+    }
 
+    if(flagSeguranca) {
+        if(flagAcionamento) {
+            if(debounceInverso(IN2)) {
+                flagAcionamento = false;
+            }
+        }
+        else {
+            if(debounce(IN2)) {
+                flagAcionamento = true;
+            }
+        }
+    }
+
+    if(flagHomeAcelerador) {
+        if(debounceInverso(IN4)) {
+            flagHomeAcelerador = false;
+        }
+    }
+    else {
+        if(debounce(IN4)) {
+            flagHomeAcelerador = true;
+        }
+    }
+
+    if(flagFimCursoAcelerador) {
+        if(debounceInverso(IN5)) {
+            flagFimCursoAcelerador = false;
+        }
+    }
+    else {
+        if(debounce(IN5)) {
+            flagFimCursoAcelerador = true;
+        }
+    }
 }
 /*==============================================================================
 FIM DO ARQUIVO
